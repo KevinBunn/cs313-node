1c0b95a2d65869a7f9763d4fcac9006e55cc0274
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-21 16:37:35 -0600

    pretty much done

--- index.js
+++ index.js
@@ -7,4 +7,31 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+    .get('/result', function(req, res) {
+
+        let type = req.query.type;
+        let weight = req.query.weight;
+        let price = 0;
+
+        switch (type) {
+            case "stamped": {
+                price = .29 + (weight * .21);
+                break;
+            }
+            case "metered": {
+                price = .26 + (weight * .21);
+                break;
+            }
+            case "flats": {
+                price = ((weight - .50) * .21) + .50;
+                break;
+            }
+            case "first-class": {
+                break;
+            }
+        }
+
+        res.locals.price = price.toPrecision(2);
+        res.render('pages/result');
+    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
