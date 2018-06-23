d343938aff71a44b2257c7e3fd21f569346594f1
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-23 09:04:52 -0600

    switch case done

--- index.js
+++ index.js
@@ -15,18 +15,62 @@ express()
 
         switch (type) {
             case "stamped": {
-                price = .29 + (weight * .21);
+                if (weight <= 3.5 && weight > 0)
+                    price = .29 + (weight * .21);
+                else
+                    res.locals.weightError = `Weight cannot be over 3.5 for ${type}`;
                 break;
             }
             case "metered": {
-                price = .26 + (weight * .21);
+                if (weight <= 3.5 && weight > 0)
+                    price = .26 + (weight * .21);
+                else
+                    res.locals.weightError = `Weight cannot be over 3.5 for ${type}`;
                 break;
             }
             case "flats": {
-                price = ((weight - .50) * .21) + .50;
+                if (weight <= 13 && weight > 0)
+                    price = ((weight - 1) * .21) + 1;
+                else
+                    res.locals.weightError = `Weight cannot be over 13 for ${type}`;
                 break;
             }
             case "first-class": {
+                weight = Math.round(weight);
+                if (weight <= 13 && weight > 0) {
+                    switch (weight) {
+                        case 1:
+                        case 2:
+                        case 3:
+                        case 4:
+                            price = 3.50;
+                            break;
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                            price = 3.75;
+                            break;
+                        case 9:
+                            price = 4.10;
+                            break;
+                        case 10:
+                            price = 4.45;
+                            break;
+                        case 11:
+                            price = 4.80;
+                            break;
+                        case 12:
+                            price = 5.15;
+                            break;
+                        case 13:
+                            price = 5.50;
+                            break;
+                    }
+                }
+                else {
+                    res.locals.weightError = `Weight cannot be over 13 for ${type}`;
+                }
                 break;
             }
         }
