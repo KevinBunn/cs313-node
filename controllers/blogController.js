e1022ec14580e79655d804c657ffb0b82fa18e40
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-29 13:20:41 -0600

    init

---
+++ controllers/blogController.js
@@ -0,0 +1,8 @@
+
+function handleBlog(req, res) {
+    res.render("pages/index");
+}
+
+module.exports = {
+    handleBlog: handleBlog
+};
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 const path = require('path');
 
+const controller = requre('./controllers/blogController');
+
 express()
     .use(express.static(path.join(__dirname, 'public')))
     .use(express.json())     // to support JSON-encoded bodies
     .use(express.urlencoded(extended = true))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
+    .get('/', controller.handleBlog)
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
