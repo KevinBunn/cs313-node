fb52fcca5ab62acb579e8121d9ea61cf00513e64
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-29 14:11:50 -0600

    stylesheet

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 const controller = require('./controllers/blogController');
 
 express()
-    .use(express.static(path.join(__dirname, 'public')))
+    .use(express.static(path.join(__dirname, '/public')))
     .use(express.json())     // to support JSON-encoded bodies
     .use(express.urlencoded(extended = true)) // mainly used for post
     .set('views', path.join(__dirname, 'views'))
