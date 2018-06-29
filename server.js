ff4fcf8a443cc026cf5181663070435c14d2bfdf
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-29 12:54:07 -0600

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require('express');
+const PORT = process.env.PORT || 5000;
+const path = require('path');
+
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .use(express.json())     // to support JSON-encoded bodies
+    .use(express.urlencoded(extended = true))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
