4d2a614a1bcd0c7da071608286b735e898b84ccb
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-29 13:22:00 -0600

    controller

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 const path = require('path');
 
-const controller = requre('./controllers/blogController');
+const controller = require('./controllers/blogController');
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
