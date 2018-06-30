8871b879961df7e8c40f4b51b2362d08f8ceca75
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-29 18:43:16 -0600

    database query

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -5,7 +5,7 @@ function handleBlog(req, res) {
         if (err)
             console.log(err)
         else {
-            res.locals.blogPostJson = results;
+            res.locals.blogPostJson = JSON.parse(results);
             res.render("pages/index");
         }
     })
