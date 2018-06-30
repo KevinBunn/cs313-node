d387eb63ce7943e8cc3780f6a61028b9bfa87673
Kevin Bunn <rocketlikeabass@gmail.com>
2018-06-30 17:01:27 -0600

    username query

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -7,10 +7,10 @@ function handleBlog(req, res) {
         else {
             console.log(JSON.stringify(results));
             let resultsJson = JSON.parse(JSON.stringify(results));
-            let user = blogModel.getUserInfo(resultsJson["rows"]["admin_id"]);
+            let username = blogModel.getUserInfo(resultsJson["rows"]["admin_id"]);
 
             res.locals.blogPostJson = resultsJson;
-            res.locals.username = user;
+            res.locals.username = username;
             res.render("pages/index");
         }
     })
