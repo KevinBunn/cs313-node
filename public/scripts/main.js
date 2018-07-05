a73cdb752e9ada8288d8802cac7166d74192f1b3
Kevin Bunn <rocketlikeabass@gmail.com>
2018-07-05 09:49:57 -0600

    signup

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -13,7 +13,7 @@ function dropdown() {
 
 window.onclick = function(event) {
     console.log(event.target);
-    if (!event.target.matches('.dropbtn') && !event.target.matches('custom-input')) {
+    if (!event.target.matches('.dropbtn') && !event.target.matches('.custom-input')) {
 
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
