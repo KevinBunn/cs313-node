f46c907c236f9fc7d5c711ee2bd55be4edb6682e
Kevin Bunn <rocketlikeabass@gmail.com>
2018-07-04 19:08:01 -0600

    display new post

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,3 +2,7 @@ function showNewPostForm() {
     let newPostForm = document.getElementById('new-post-container');
     newPostForm.setAttribute('style','display: block');
 }
+function hideNewPostForm() {
+    let newPostForm = document.getElementById('new-post-container');
+    newPostForm.setAttribute('style','display: none');
+}
