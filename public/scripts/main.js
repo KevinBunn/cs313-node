711c5aadb48f5885dc9c99dd5a5fc261432883d0
Kevin Bunn <rocketlikeabass@gmail.com>
2018-07-04 20:44:44 -0600

    signup

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -10,3 +10,17 @@ function hideNewPostForm() {
 function dropdown() {
     document.getElementById("dropdown-menu").classList.add('show');
 }
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
