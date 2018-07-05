65beb93f972ef43ba451f270e45603b986152913
Kevin Bunn <rocketlikeabass@gmail.com>
2018-07-04 19:45:19 -0600

    signup

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,3 +6,7 @@ function hideNewPostForm() {
     let newPostForm = document.getElementById('new-post-container');
     newPostForm.setAttribute('style','display: none');
 }
+
+function dropdown(categoryId) {
+    document.getElementById("dropdown-menu").classList.add('show');
+}
