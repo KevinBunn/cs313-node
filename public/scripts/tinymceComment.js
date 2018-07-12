140a8fdac9825961a6bbf8af9639294012e50b31
Kevin Bunn <rocketlikeabass@gmail.com>
2018-07-12 13:31:51 -0600

    tinymce me now?

--- public/scripts/tinymceComment.js
+++ public/scripts/tinymceComment.js
@@ -1,11 +1,5 @@
 tinymce.init({
     selector: '.add-comment-area',
     menubar: false,
-    formats: {
-        removeformat: [
-            {selector: 'b,strong,em,i,font,u,strike', remove : 'all', split : true, expand : false, block_expand: true, deep : true},
-            {selector: 'span', attributes : ['style', 'class'], remove : 'empty', split : true, expand : false, deep : true},
-            {selector: '*', attributes : ['style', 'class'], split : false, expand : false, deep : true}
-        ]
-    }
+    status: false
 });
