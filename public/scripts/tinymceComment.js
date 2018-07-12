231392168d08b0e3886db7e831a3c5632b72ad3b
Kevin Bunn <rocketlikeabass@gmail.com>
2018-07-12 13:37:10 -0600

    tinymce me now?

--- public/scripts/tinymceComment.js
+++ public/scripts/tinymceComment.js
@@ -2,5 +2,7 @@ tinymce.init({
     selector: '.add-comment-area',
     menubar: false,
     statusbar: false,
-    toolbar: false
+    toolbar: false,
+    plugins: "paste, image",
+    paste_as_text: true
 });
