b06948f06ef086ecd3b3a9deb68135133e1674e8
Kevin Bunn <rocketlikeabass@gmail.com>
2018-07-12 14:41:03 -0600

    comment section

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -172,6 +172,7 @@ function addPost() {
 
 function addComment(postId) {
     let content = tinymce.get("add-comment-area").getContent();
+    console.log(content);
     callAjaxComment(`/addComment`, postId, content, function(res) {
         console.log(`back from ajax call with response: ${res}`);
         resJson = JSON.parse(res);
